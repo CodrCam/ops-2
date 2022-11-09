d222d257b129be1bb50af24348cc3d4680ec275d
Cameron Griffin (He/Him) <101752687+CodrCam@users.noreply.github.com>
2022-11-08 20:34:29 -0800

    Create lab2.sh

---
+++ lab2.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#Script: Ops 201 Class 02 Ops Challenge Solution
+#Author: Cameron Griffin
+#Date of latest revision: 11-8-2022
+#Purpose: Print a string into the terminal
+
+
+#Main
+
+var="I am working perfectly"
+
+echo $var
+
+#End
